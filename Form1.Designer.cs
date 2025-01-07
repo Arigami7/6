13a21b749318f83f94531e5be6c9cf13c0210df9
Arigami7 <artem.mikheev.2018@mail.ru>
2025-01-08 01:08:36 +0500

    init2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,6 +52,19 @@ namespace MikheevProject6
             this.textBoxPointY1 = new System.Windows.Forms.TextBox();
             this.label12 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.textBoxPointY2 = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.textBoxPointX2 = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -66,7 +79,7 @@ namespace MikheevProject6
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Location = new System.Drawing.Point(114, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 1;
@@ -74,14 +87,14 @@ namespace MikheevProject6
             // 
             // textBoxX1
             // 
-            this.textBoxX1.Location = new System.Drawing.Point(51, 39);
+            this.textBoxX1.Location = new System.Drawing.Point(149, 41);
             this.textBoxX1.Name = "textBoxX1";
             this.textBoxX1.Size = new System.Drawing.Size(42, 20);
             this.textBoxX1.TabIndex = 2;
             // 
             // textBoxY1
             // 
-            this.textBoxY1.Location = new System.Drawing.Point(154, 39);
+            this.textBoxY1.Location = new System.Drawing.Point(252, 41);
             this.textBoxY1.Name = "textBoxY1";
             this.textBoxY1.Size = new System.Drawing.Size(42, 20);
             this.textBoxY1.TabIndex = 4;
@@ -89,7 +102,7 @@ namespace MikheevProject6
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(119, 42);
+            this.label3.Location = new System.Drawing.Point(217, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 13);
             this.label3.TabIndex = 3;
@@ -97,7 +110,7 @@ namespace MikheevProject6
             // 
             // textBoxX2
             // 
-            this.textBoxX2.Location = new System.Drawing.Point(51, 78);
+            this.textBoxX2.Location = new System.Drawing.Point(149, 80);
             this.textBoxX2.Name = "textBoxX2";
             this.textBoxX2.Size = new System.Drawing.Size(42, 20);
             this.textBoxX2.TabIndex = 6;
@@ -105,7 +118,7 @@ namespace MikheevProject6
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 81);
+            this.label4.Location = new System.Drawing.Point(114, 83);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 5;
@@ -113,7 +126,7 @@ namespace MikheevProject6
             // 
             // textBoxX3
             // 
-            this.textBoxX3.Location = new System.Drawing.Point(51, 117);
+            this.textBoxX3.Location = new System.Drawing.Point(149, 119);
             this.textBoxX3.Name = "textBoxX3";
             this.textBoxX3.Size = new System.Drawing.Size(42, 20);
             this.textBoxX3.TabIndex = 8;
@@ -121,7 +134,7 @@ namespace MikheevProject6
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 120);
+            this.label5.Location = new System.Drawing.Point(114, 122);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 7;
@@ -129,7 +142,7 @@ namespace MikheevProject6
             // 
             // textBoxX4
             // 
-            this.textBoxX4.Location = new System.Drawing.Point(51, 154);
+            this.textBoxX4.Location = new System.Drawing.Point(149, 156);
             this.textBoxX4.Name = "textBoxX4";
             this.textBoxX4.Size = new System.Drawing.Size(42, 20);
             this.textBoxX4.TabIndex = 10;
@@ -137,7 +150,7 @@ namespace MikheevProject6
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 157);
+            this.label6.Location = new System.Drawing.Point(114, 159);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(29, 13);
             this.label6.TabIndex = 9;
@@ -145,7 +158,7 @@ namespace MikheevProject6
             // 
             // textBoxY2
             // 
-            this.textBoxY2.Location = new System.Drawing.Point(154, 78);
+            this.textBoxY2.Location = new System.Drawing.Point(252, 80);
             this.textBoxY2.Name = "textBoxY2";
             this.textBoxY2.Size = new System.Drawing.Size(42, 20);
             this.textBoxY2.TabIndex = 12;
@@ -153,7 +166,7 @@ namespace MikheevProject6
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(119, 81);
+            this.label7.Location = new System.Drawing.Point(217, 83);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(29, 13);
             this.label7.TabIndex = 11;
@@ -161,7 +174,7 @@ namespace MikheevProject6
             // 
             // textBoxY3
             // 
-            this.textBoxY3.Location = new System.Drawing.Point(154, 117);
+            this.textBoxY3.Location = new System.Drawing.Point(252, 119);
             this.textBoxY3.Name = "textBoxY3";
             this.textBoxY3.Size = new System.Drawing.Size(42, 20);
             this.textBoxY3.TabIndex = 14;
@@ -169,7 +182,7 @@ namespace MikheevProject6
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(119, 120);
+            this.label8.Location = new System.Drawing.Point(217, 122);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(29, 13);
             this.label8.TabIndex = 13;
@@ -177,7 +190,7 @@ namespace MikheevProject6
             // 
             // textBoxY4
             // 
-            this.textBoxY4.Location = new System.Drawing.Point(154, 154);
+            this.textBoxY4.Location = new System.Drawing.Point(252, 156);
             this.textBoxY4.Name = "textBoxY4";
             this.textBoxY4.Size = new System.Drawing.Size(42, 20);
             this.textBoxY4.TabIndex = 16;
@@ -185,7 +198,7 @@ namespace MikheevProject6
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(119, 157);
+            this.label9.Location = new System.Drawing.Point(217, 159);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(29, 13);
             this.label9.TabIndex = 15;
@@ -194,7 +207,7 @@ namespace MikheevProject6
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(16, 196);
+            this.label10.Location = new System.Drawing.Point(13, 192);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(147, 13);
             this.label10.TabIndex = 17;
@@ -234,18 +247,149 @@ namespace MikheevProject6
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 263);
+            this.button1.Location = new System.Drawing.Point(114, 386);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 23);
             this.button1.TabIndex = 22;
             this.button1.Text = "Определить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(13, 420);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(62, 13);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "Результат:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(19, 272);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(230, 17);
+            this.radioButton1.TabIndex = 25;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Демонстрация работы ссылочного типа";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(19, 295);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(239, 17);
+            this.radioButton2.TabIndex = 26;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Демонстрация работы развёрнутого типа";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(13, 44);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(95, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Правая вершина:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(13, 83);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(89, 13);
+            this.label15.TabIndex = 28;
+            this.label15.Text = "Левая вершина:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(13, 122);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(99, 13);
+            this.label16.TabIndex = 29;
+            this.label16.Text = "Верхняя вершина:";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(13, 159);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(97, 13);
+            this.label17.TabIndex = 30;
+            this.label17.Text = "Нижняя вершина:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(13, 451);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(41, 13);
+            this.label18.TabIndex = 31;
+            this.label18.Text = "label18";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(16, 335);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(159, 13);
+            this.label19.TabIndex = 32;
+            this.label19.Text = "Изменяемые значения точки:";
+            // 
+            // textBoxPointY2
+            // 
+            this.textBoxPointY2.Location = new System.Drawing.Point(154, 360);
+            this.textBoxPointY2.Name = "textBoxPointY2";
+            this.textBoxPointY2.Size = new System.Drawing.Size(42, 20);
+            this.textBoxPointY2.TabIndex = 36;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(119, 363);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(23, 13);
+            this.label20.TabIndex = 35;
+            this.label20.Text = "Y =";
+            // 
+            // textBoxPointX2
+            // 
+            this.textBoxPointX2.Location = new System.Drawing.Point(51, 360);
+            this.textBoxPointX2.Name = "textBoxPointX2";
+            this.textBoxPointX2.Size = new System.Drawing.Size(42, 20);
+            this.textBoxPointX2.TabIndex = 34;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(16, 363);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(23, 13);
+            this.label21.TabIndex = 33;
+            this.label21.Text = "X =";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(312, 550);
+            this.Controls.Add(this.textBoxPointY2);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.textBoxPointX2);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label13);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxPointY1);
             this.Controls.Add(this.label12);
@@ -301,6 +445,19 @@ namespace MikheevProject6
         private System.Windows.Forms.TextBox textBoxPointY1;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox textBoxPointY2;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox textBoxPointX2;
+        private System.Windows.Forms.Label label21;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,13 +12,88 @@ namespace MikheevProject6
 {
     public partial class Form1 : Form
     {
-        private Point point;
-        private Rhomb rhomb;
-        PointStruct pointStruct;
-        RhombStruct rhombStruct;
         public Form1()
         {
             InitializeComponent();
         }
+
+        static public bool IsValidRhomb(List<Point> points, out Rhomb outrhomb)
+        {
+            double highestY = double.MinValue;
+            double lowestY = double.MaxValue;
+            double rightmostX = double.MinValue;
+            double leftmostX = double.MaxValue;
+            Point topPoint = points[0];
+            Point bottomPoint = points[0];
+            Point rightPoint = points[0];
+            Point leftPoint = points[0];
+            foreach (Point item in points)
+            {
+                if (item.X > rightmostX)
+                {
+                    rightmostX = item.X;
+                    rightPoint = item;
+                }
+                if (item.X < leftmostX)
+                {
+                    leftmostX = item.X;
+                    leftPoint = item;
+                }
+                if (item.Y > highestY)
+                {
+                    highestY = item.Y;
+                    topPoint = item;
+                }
+                if (item.Y < lowestY)
+                {
+                    lowestY = item.Y;
+                    bottomPoint = item;
+                }
+            }
+
+            bool lr = leftPoint.IsOnSameY(rightPoint);
+            bool tb = topPoint.IsOnSameX(bottomPoint);
+
+            if (lr && tb && rightPoint.GetDistance(topPoint) == topPoint.GetDistance(leftPoint) && 
+                topPoint.GetDistance(leftPoint) == leftPoint.GetDistance(bottomPoint) && 
+                leftPoint.GetDistance(bottomPoint) == bottomPoint.GetDistance(rightPoint))
+            {
+                Rhomb rhomb = new Rhomb(topPoint, bottomPoint, leftPoint, rightPoint);
+                outrhomb = rhomb;
+                return true;
+            }
+            else
+            {
+                outrhomb = null;
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (radioButton1.Checked)
+                {
+
+                }
+                else if (radioButton2.Checked)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Выберите параметр!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введены некорректные значения!");
+            }
+        }
     }
 }
+
+
+      
